6fdecf122aee6b02d6c195ba81f500f9e9a71138
Zamirzamir44 <tooradicalxxi@gmail.com>
2020-12-11 18:23:30 +0100

    clock exercise finished

--- drumkit/script.js
+++ drumkit/script.js
@@ -1,6 +1,24 @@
-window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    if(!audio) return;
-    audio.currentTime = 0; 
-    audio.play();
-})
+
+
+function playSound(e) {
+    
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        const key  = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+        if(!audio) return;
+        audio.currentTime = 0; 
+        audio.play();
+        console.log(key);
+        key.classList.add('playing');
+        
+    
+}
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound)
